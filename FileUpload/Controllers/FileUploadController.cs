e545d7bf37aa59c7f86fca69935f4e47d98ce749
Garrett Wong <wong_ga@sc.wdc.com>
2016-10-19 02:06:00 -0700

    initial commit

---
+++ FileUpload/Controllers/FileUploadController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Http;
+
+namespace FileUpload.Controllers
+{
+    public class FileUploadController : ApiController
+    {
+        public FileUploadController()
+        {
+
+        }
+
+        [Route("api/File/{type}")]
+        public HttpResponseMessage Post(string type)
+        {
+            var httpRequest = HttpContext.Current.Request;
+
+            var uploadPath = ConfigurationManager.AppSettings["UploadPath"];
+
+            Directory.CreateDirectory(uploadPath);
+            foreach (string fileKeyName in httpRequest.Files)
+            {
+                HttpPostedFile file = httpRequest.Files[fileKeyName];
+                string filename = file.FileName;
+                string extension = file.ContentType;
+
+                if (!string.IsNullOrEmpty(filename))
+                {
+                    extension = Path.GetExtension(filename);
+                    var renamedFile = type;
+                    file.SaveAs(uploadPath + "\\" + renamedFile);
+                }
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+
+        [Route("api/File")]
+        public HttpResponseMessage Get(string filename)
+        {
+            try
+            {
+                var response = new HttpResponseMessage();
+                
+                var uploadPath = ConfigurationManager.AppSettings["UploadPath"];
+                response = new HttpResponseMessage();
+
+                byte[] filedata = File.ReadAllBytes(uploadPath + "\\" + filename);
+                response.Content = new ByteArrayContent(filedata);
+                response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
+                {
+                    FileName = filename
+                };
+
+                response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+                return response;
+            }
+            catch (Exception e)
+            {
+
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ FileUpload/Controllers/FileUploadRestSharpTest_Controller.cs
@@ -0,0 +1,38 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FileUpload.Controllers
+{
+    public class FileUploadRestSharpTest_Controller : ApiController
+    {
+        [HttpGet]
+
+        [Route("api/FileUploadRestSharpTest")]
+        public void Get()
+        {
+            var client = new RestClient("http://localhost:54320/api/FileUploadRestSharpTest");
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("postman-token", "ac584f0a-4534-0278-bd2b-e4b1b9c6995a");
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("content-type", "multipart/form-data; boundary=---011000010111000001101001");
+            request.AddParameter("multipart/form-data; boundary=---011000010111000001101001", "-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"g\"; filename=\"[object Object]\"\r\nContent-Type: false\r\n\r\n\r\n-----011000010111000001101001--", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+        }
+
+        [HttpGet]
+        [Route("api/FileUploadRestSharpTest")]
+        public void Post()
+        {
+            var client = new RestClient("http://localhost:54320/api/File/hello");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("content-type", "multipart/form-data; boundary=---011000010111000001101001");
+            request.AddParameter("multipart/form-data; boundary=---011000010111000001101001", "-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"g\"; filename=\"[object Object]\"\r\nContent-Type: false\r\n\r\n\r\n-----011000010111000001101001--", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+        }
+    }
+}
