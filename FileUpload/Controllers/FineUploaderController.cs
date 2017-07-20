ea91f39b35ea4af2494b64ec53d7a82fa3029fe0
Garrett <garrettwong@outlook.com>
2017-07-20 11:32:33 -0700

    sync with file upload

---
+++ FileUpload/Controllers/FineUploaderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileUpload.Controllers
+{
+    public class FineUploaderController : Controller
+    {
+        // GET: FineUploader
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
