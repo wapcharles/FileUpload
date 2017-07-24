8720891038f520eb5d0e5d439a0851f9b8c5a0ad
SC\corpus_c <charles.corpus@wdc.com>
2017-07-24 15:21:29 -0700

    for garrett

---
+++ FileUpload/Controllers/RadAsyncUploadController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileUpload.Controllers
+{
+    public class RadAsyncUploadController : Controller
+    {
+        // GET: RadAsyncUpload
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
